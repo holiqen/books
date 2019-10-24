7e06c8b5fda50dfd42a1b77776033abb8857576e
Anatoliy Kireev <bug.tlp@gmail.com>
2019-10-24 15:11:39 +0300

    Fix jsdoc for listType of book data

--- src/actions/books.js
+++ src/actions/books.js
@@ -2,7 +2,7 @@
  * Действие "Добавлеие книги"
  * @param {Object} bookData
  * @param {String} bookData.title Заголовок книги
- * @param {String} bookData.listType Тип списка (read или complete) для хранения книги
+ * @param {'read'|'complete'} bookData.listType Тип списка (read или complete) для хранения книги
  */
 export const addBook = (bookData) => ({
   type: "ADD_BOOK",
