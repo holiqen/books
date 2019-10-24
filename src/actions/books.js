64e6ba2319dcac836dac185a4ba37ab339840f6a
Anatoliy Kireev <bug.tlp@gmail.com>
2019-10-24 15:39:23 +0300

    Delete line between func and jsdoc

--- src/actions/books.js
+++ src/actions/books.js
@@ -4,8 +4,7 @@
  * @param {String} bookData.title Заголовок книги
  * @param {'read'|'complete'} bookData.listType Тип списка (read или complete) для хранения книги
  */
-
 export const addBook = (bookData) => ({
-    type: "ADD_BOOK",
-    payload: bookData,
+  type: "ADD_BOOK",
+  payload: bookData,
 });
